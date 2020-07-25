dba43c8ffc832e3097cb8214f4a510b839b25593
Pablo Menz <p.menz.r@gmail.com>
2020-07-25 05:15:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,4 +14,16 @@ def assign_rooms(attendees)
   attendees.each_with_index.map do |element,index| 
     "Hello, #{element}! You'll be assigned to room #{index+1}!"
   end
+def printer(attendees)
+  batch_badge_creator(attendees).each do |element| 
+    puts element
+  end
+end
+
+
+
+
+
+
+
 end
